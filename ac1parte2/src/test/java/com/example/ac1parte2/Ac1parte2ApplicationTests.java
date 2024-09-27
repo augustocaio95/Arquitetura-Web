27405f9f7b9d6fd2a69fb5dc9747f41472068e70
Caio Augusto <124223509+augustocaio95@users.noreply.github.com>
2024-09-26 21:36:17 -0300

    Add files via upload

---
+++ ac1parte2/src/test/java/com/example/ac1parte2/Ac1parte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ac1parte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac1parte2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
